64317985fbc675730272519803b8cd63eb59987d
Karabinar <karabinar.dwika@gmail.com>
2016-04-05 20:35:46 +0700

    make another java class

--- src/main/java/com/bl/rekweb/ku/group6/Car.java
+++ src/main/java/com/bl/rekweb/ku/group6/Car.java
@@ -4,4 +4,21 @@ package com.bl.rekweb.ku.group6;
  * Created by karabinar on 05-Apr-16.
  */
 public class Car {
+
+    private int tire = 2;
+    private int door;
+
+    public static void main(String[]Args){
+        String tire = "Hitam";
+        System.out.println("karabinar");
+        System.out.println(tire);
+    }
+
+    public void forward(){
+
+    }
+
+    public void backward(){
+
+    }
 }
